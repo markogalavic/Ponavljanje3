5718a7bcf95a2493aca9acf509ac4a1362d42b15
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-11-12 11:01:28 +0100

    Add files via upload

---
+++ markoGalavicPonavljanje3/markoGalavicPonavljanje3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavicPonavljanje3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] niz = new string[3];
+
+            Console.WriteLine("Unesi prvi string");
+            niz[0] = Console.ReadLine();
+            Console.WriteLine("Unesi drugi string");
+            niz[1] = Console.ReadLine();
+            Console.WriteLine("Unesi treci string ");
+            niz[2] = Console.ReadLine();
+
+            Array.Sort(niz);
+            Array.Reverse(niz);
+            Console.WriteLine("Sortirani stringovi");
+            for(int i=0; i<2; i++)
+            {
+                Console.WriteLine(niz[1]);
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
